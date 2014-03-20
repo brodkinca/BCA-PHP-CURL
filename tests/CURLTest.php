8a6c3060bc9f22f9fdba8d965ea4898b88a1387d
Ryan Brodkin <ryan@brodkinca.com>
2014-03-19 23:09:13 -0700

    Removed unused methods from CURL class.

--- tests/CURLTest.php
+++ tests/CURLTest.php
@@ -32,7 +32,6 @@ class CURLTest extends \PHPUnit_Framework_TestCase
      * @covers BCA\CURL\CURL::get
      * @covers BCA\CURL\CURL::__construct
      * @covers BCA\CURL\CURL::_startSession
-     * @covers BCA\CURL\CURL::_options
      * @covers BCA\CURL\CURL::_hasExtCurl
      * @covers BCA\CURL\CURL::_hasOption
      * @covers BCA\CURL\CURL::_method
@@ -62,7 +61,6 @@ class CURLTest extends \PHPUnit_Framework_TestCase
      * @covers BCA\CURL\CURL::post
      * @covers BCA\CURL\CURL::__construct
      * @covers BCA\CURL\CURL::_startSession
-     * @covers BCA\CURL\CURL::_options
      * @covers BCA\CURL\CURL::_hasExtCurl
      * @covers BCA\CURL\CURL::_hasOption
      * @covers BCA\CURL\CURL::_method
@@ -100,7 +98,6 @@ class CURLTest extends \PHPUnit_Framework_TestCase
      * @covers BCA\CURL\CURL::put
      * @covers BCA\CURL\CURL::__construct
      * @covers BCA\CURL\CURL::_startSession
-     * @covers BCA\CURL\CURL::_options
      * @covers BCA\CURL\CURL::_hasExtCurl
      * @covers BCA\CURL\CURL::_hasOption
      * @covers BCA\CURL\CURL::_method
@@ -137,7 +134,6 @@ class CURLTest extends \PHPUnit_Framework_TestCase
      * @covers BCA\CURL\CURL::delete
      * @covers BCA\CURL\CURL::__construct
      * @covers BCA\CURL\CURL::_startSession
-     * @covers BCA\CURL\CURL::_options
      * @covers BCA\CURL\CURL::_hasExtCurl
      * @covers BCA\CURL\CURL::_hasOption
      * @covers BCA\CURL\CURL::_method
