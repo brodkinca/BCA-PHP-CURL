2060b1d40b01c972208f5884a22e8d543127cdb7
Ryan Brodkin <ryan@brodkinca.com>
2014-03-24 10:53:41 -0700

    Fix bug in which CI builds would fail due to undefined SSL_CERT_PATH.

---
+++ tests/bootstrap.ci.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * cURL Library
+ *
+ * PHP Version 5.3
+ *
+ * @category  Library
+ * @package   BCA/CURL
+ * @author    Brodkin CyberArts <support@brodkinca.com>
+ * @copyright 2012 Brodkin CyberArts.
+ * @license   GPL-3.0 http://www.gnu.org/licenses/gpl.txt
+ * @version   GIT: $Id$
+ * @link      https://github.com/brodkinca/BCA-PHP-CURL
+ */
+
+// Load Dependencies
+require_once 'vendor/autoload.php';
+
+define('SSL_CERT_PATH', __DIR__.'/remote/server.pem');
